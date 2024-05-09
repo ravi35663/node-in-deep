1902d9937ff884bc64e2f79116fb1c2ceae8d5a3
ravi-kumar5-kiwitech <ravi.kumar5@kiwitech.com>
2024-05-09 18:50:08 +0530

    learning repl

--- new-test.js
+++ new-test.js
@@ -1,7 +1,13 @@
+console.log("Hello World")
+
+const fs = require('fs')
+// Read stream
+const readStream = fs.createReadStream('./some_file.txt')
+
+
 // const fs = require('fs')
 // // Read stream
 // const readStream = fs.createReadStream('./some_file.txt')
 
 // readStream.readStream
 
-console.log("Hello Wold")
