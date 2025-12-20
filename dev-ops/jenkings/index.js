f3d2b8848722a82786fc0565cb3bc7472242a30f
ravi <ravikr845430@gmail.com>
2025-12-20 20:12:42 +0530

    learning aws

---
+++ dev-ops/jenkings/index.js
@@ -0,0 +1,20 @@
+/*
+==> Jenkins:
+    ->  Jenkins is a open source automation server/tool.
+    ->  With the help of Jenkins we can automate our tasks.
+*/
+
+/*
+==> CI/CD:
+    ->  Continuous integration/ Continuous Delivery/Deployment.
+    ->  Continuous Integration:
+        ->  These are the stuff we do in CI:
+            ->  Build
+            ->  Test
+            ->  Merge
+    ->  Continuous Delivery:
+        ->  Automatically release to repository
+    ->  Continuous Deployment:
+        ->  Automatically Deploy to production.
+
+*/
